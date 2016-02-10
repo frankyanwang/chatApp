12fcc1d15d5b9cb6db686c865a400404ab3091fa
Frank Wang <fwang@vlocity.com>
2016-02-09 17:54:34 -0800

    Render card style contact detail page.

--- www/app/contact/contact-controller.js
+++ www/app/contact/contact-controller.js
@@ -5,10 +5,10 @@
         .module('vlocityApp')
         .controller('ContactController', ContactController);
 
-    ContactController.$inject = ['force', '$stateParams'];
+    ContactController.$inject = ['force', '$stateParams','$http'];
 
     /* @ngInject */
-    function ContactController(force, $stateParams) {
+    function ContactController(force, $stateParams, $http) {
         var vm = this;
         vm.property = 'ContactController';
 
@@ -21,6 +21,11 @@
             force.retrieve('contact', $stateParams.contactId, 'id,name,title,phone,mobilephone,email').then(
                 function (contact) {
                     vm.contact = contact;
+                    $http.get("http://uifaces.com/api/v1/random").then(function (response) {
+                        console.log(response);
+                        // epic, bigger, normal, mini
+                        vm.contact.imgUrl = response.data.image_urls.epic;
+                    });
                 });
         }
     }
