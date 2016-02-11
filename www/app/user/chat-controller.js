9c7f1b10a3e52eb8e84794f47fa241f1ab174a66
Frank Wang <fwang@vlocity.com>
2016-02-10 16:29:33 -0800

    Send contact card is now working.  Syntax is #contact @name@  TODO: make other subject type working.

--- www/app/user/chat-controller.js
+++ www/app/user/chat-controller.js
@@ -5,23 +5,59 @@
         .module('vlocityApp')
         .controller('ChatController', ChatController);
 
-    ChatController.$inject = ['$stateParams', 'chatFactory', '$rootScope'];
+    ChatController.$inject = ['$stateParams', 'chatFactory', '$rootScope', 'force'];
 
     /* @ngInject */
-    function ChatController($stateParams, chatFactory, $rootScope) {
+    function ChatController($stateParams, chatFactory, $rootScope, force) {
         var vm = this;
         vm.property = 'ChatController';
         // model bind to template
         vm.currentChat = '';
         vm.myId = $rootScope.currentUser.Id;
         vm.hideTime = true;
-        
+
         vm.chatUserName = $stateParams.userName;
         vm.chatUserId = $stateParams.userId;
-        
+
         vm.sendMessage = function () {
             if (!vm.currentChat || vm.currentChat.length == 0) {
                 return;
+            }
+
+            var index = vm.currentChat.indexOf('#contact');
+            if (index >= 0) {
+                var results = vm.currentChat.match(/@([A-Za-z0-9_ ]+)@/);
+                if (results && results.length > 0) {
+                    var contactName = results[1];
+
+                    force.query("select id,name,title,phone,mobilephone,email FROM contact where name like '%" + contactName + "%'").then(
+                        function (data) {
+                            if (data.records.length == 0) {
+                                alert("cannot find contact - " + contactName);
+                            }
+                            if (data.records.length == 1) {
+                                var contact = data.records[0];
+                                vm.chats.$add({
+                                    userId: $rootScope.currentUser.Id,
+                                    name: $rootScope.currentUser.Name,
+                                    email: $rootScope.currentUser.Email,
+                                    type: "CONTACT",
+                                    contact: contact,
+                                    timestamp: Firebase.ServerValue.TIMESTAMP
+                                }).then(function () {
+                                    vm.currentChat = '';
+                                });
+                            } else {
+                                alert("Find more than one contact - " + contactName);
+                            }
+                        },
+                        function (error) {
+                            alert("cannot find contact - " + contactName);
+                            console.log(error);
+                        });
+
+
+                }
             }
 
             vm.chats.$add({
